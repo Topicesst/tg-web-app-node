1426661266cad72730ae582a459a741a905abfd9
Topicesst <132078398+Topicesst@users.noreply.github.com>
2024-03-23 15:05:27 +0200

    index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.on('message', async (msg) => {
             }
         })
 
-        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+        await bot.sendMessage(chatId, 'Заходи ТУК ТУК в наш интернет магазин по кнопке ниже', {
             reply_markup: {
                 inline_keyboard: [
                     [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
