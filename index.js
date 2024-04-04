068c7fcef0ab928861f5d6dea8b82e4de2e51360
Topicesst <132078398+Topicesst@users.noreply.github.com>
2024-04-04 23:45:47 +0300

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 
-const token = '6702075740:AAEDAjNrX1hVS5TJd9NqFYr-8FmQpWY0Lm0';
+const token = 'YOUR_TELEGRAM_BOT_TOKEN'; // Замініть на ваш токен
 const webAppUrl = 'https://deft-caramel-01f656.netlify.app/';
 
 const bot = new TelegramBot(token, { polling: true });
@@ -19,8 +19,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+let lastChatId = null;
+
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
+  lastChatId = chatId; 
+
   const text = msg.text;
 
   if (text === '/start') {
@@ -32,27 +36,18 @@ bot.on('message', async (msg) => {
         one_time_keyboard: true
       }
     });
-  }
-
-  if (msg?.web_app_data?.data) {
+  } else if (text.match(/\/echo (.+)/)) {
+    const [, echoText] = text.match(/\/echo (.+)/);
+    await bot.sendMessage(chatId, `Ви сказали: ${echoText}`);
+  } else if (msg?.web_app_data?.data) {
     try {
       const data = JSON.parse(msg.web_app_data.data);
-     await bot.sendMessage(chatId, '*Дякуємо за надану інформацію!*', { parse_mode: 'Markdown' });
-     await bot.sendMessage(chatId, `*Ваше ПІБ:* _${data?.name}_`, { parse_mode: 'Markdown' });
-     await bot.sendMessage(chatId, `*Ваш номер телефону:* _${data?.numberphone}_`, { parse_mode: 'Markdown' });
-     await bot.sendMessage(chatId, `*Ваше місто:* _${data?.city}_`, { parse_mode: 'Markdown' });
-     await bot.sendMessage(chatId, `*Ваша адреса:* _${data?.street}_`, { parse_mode: 'Markdown' });
-     await bot.sendMessage(chatId, `*Вартість доставки:* _${data?.deliveryPrice}_`, { parse_mode: 'Markdown' });
-
-      setTimeout(async () => {
-        await bot.sendMessage(chatId, 'Заходьте в наш інтернет магазин за кнопкою нижче', {
-          reply_markup: {
-            inline_keyboard: [
-              [{ text: 'Зробити замовлення', web_app: { url: webAppUrl } }],
-            ]
-          }
-        });
-      }, 3000); 
+      await bot.sendMessage(chatId, '*Дякуємо за надану інформацію!*', { parse_mode: 'Markdown' });
+      await bot.sendMessage(chatId, `*Ваше ПІБ:* _${data?.name}_`, { parse_mode: 'Markdown' });
+      await bot.sendMessage(chatId, `*Ваш номер телефону:* _${data?.numberphone}_`, { parse_mode: 'Markdown' });
+      await bot.sendMessage(chatId, `*Ваше місто:* _${data?.city}_`, { parse_mode: 'Markdown' });
+      await bot.sendMessage(chatId, `*Ваша адреса:* _${data?.street}_`, { parse_mode: 'Markdown' });
+      await bot.sendMessage(chatId, `*Вартість доставки:* _${data?.deliveryPrice}_`, { parse_mode: 'Markdown' });
     } catch (e) {
       console.error(e);
     }
@@ -69,13 +64,18 @@ app.post('/web-data', async (req, res) => {
       input_message_content: {
         message_text: [
           '*Вітаємо з покупкою!*',
-          `*Cума замовлення:* _${totalPrice}₴_`,
+          `*Сума замовлення:* _${totalPrice}₴_`,
           '*Що саме ви замовили:*',
-          ...products.map(item => `• _${item.title}_`)
+          ...products.map(item => `• _${item.title}_`).join('\n')
         ].join('\n'),
-        parse_mode: 'Markdown' 
+        parse_mode: 'Markdown'
       }
     });
+
+    if (lastChatId) {
+      await bot.sendMessage(lastChatId, `*Загальна сума замовлення включаючи доставку:* _${totalPrice}₴_`, { parse_mode: 'Markdown' });
+    }
+
     res.status(200).json({});
   } catch (e) {
     console.error(e);
