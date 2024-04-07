f4e4b7707a77dd2549e0c75ece6c533655199514
Topicesst <topicesstofficial@gmail.com>
2024-04-07 17:09:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -190,7 +190,6 @@ app.post("/web-data", async (req, res) => {
   }
 });
 
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
